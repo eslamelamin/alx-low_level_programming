b80479838622660c81387edfaa37dd8900c5e0b0
eslamelamin <sfirtalaslam@gmail.com>
2022-10-18 08:53:21 -0700

    putchar

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *ives the absolute value
+ *
+ *Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
