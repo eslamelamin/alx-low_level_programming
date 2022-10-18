d93198486692cf7b4bd893e431e555657d3cbb75
eslamelamin <sfirtalaslam@gmail.com>
2022-10-18 10:19:10 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,32 +1,22 @@
 #include "main.h"
-
 /**
- *  _islower - Shows 1 if the input
+ * _islower - Short description, single line
  *
- * Return: 1 for lowercase. 0 for the rest.
+ * @c: contains value to be compared
+ * Return: Always 0.
  */
-int main(void)
-{
-	int r;
-
-	r = _islower('H');
-	_putchar(r + '0');
-        r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-        _putchar(r + '0');
-    	_putchar('\n');
-        return (0);
-}
 int _islower(int c)
+
 {
-	if (c >= 97 && c <= 122)
+
+	if (c > 'a' && c < 'z')
 	{
 		return (1);
 	}
+
 	else
 	{
 		return (0);
 	}
-	_putchar('\n');
+
 }
