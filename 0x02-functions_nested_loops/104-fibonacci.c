5fb086de2aaa3ca98c56a6e7df0cab410d0b7ad7
eslamelamin <sfirtalaslam@gmail.com>
2022-10-19 12:51:58 -0700

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -30,7 +30,6 @@ int main(void)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-
 		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
@@ -38,7 +37,6 @@ int main(void)
 		}
 
 		printf("%lu%lu", half1, half2);
-
 		if (count != 98)
 			printf(", ");
 
@@ -47,8 +45,6 @@ int main(void)
 		fib2_half1 = half1;
 		fib2_half2 = half2;
 	}
-
 	printf("\n");
-
 	return (0);
 }
