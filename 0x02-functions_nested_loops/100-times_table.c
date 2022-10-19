a68b759be10c5f1540a8ed15cd35e4b56e481b27
eslamelamin <sfirtalaslam@gmail.com>
2022-10-19 11:46:28 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,6 +24,7 @@ void print_times_table(int n)
 
 				if (prod <= 99)
 					_putchar(' ');
+
 				if (prod <= 9)
 					_putchar(' ');
 
@@ -32,6 +33,7 @@ void print_times_table(int n)
 					_putchar((prod / 100) + '0');
 					_putchar(((prod / 10)) % 10 + '0');
 				}
+
 				else if (prod <= 99 && prod >= 10)
 				{
 					_putchar((prod / 10) + '0');
