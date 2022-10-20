a3bdadc69958ef0de74af6de3888df06144ab793
eslamelamin <sfirtalaslam@gmail.com>
2022-10-20 12:06:50 -0700

    Task0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/*
- * _isupper - uppercase letters
- *  @c: character to check
+/**
+ *  _isupper - uppercase letters
+ * @c: character to check
  *
- *  Return: 0 or 1
+ * Return: 0 or 1
  */
 int _isupper(int c)
 {
