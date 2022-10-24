7318e25ecb1ca12219fc7e23d20d0e82636b764d
eslamelamin <sfirtalaslam@gmail.com>
2022-10-24 12:20:17 -0700

    task8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,8 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * print_array - prints n element of an array of integers
+ * Owned by Bwave
+ * @a: int to check
+ * @n: int to check
  * Return: 0 is success
  */
 void print_array(int *a, int n)
