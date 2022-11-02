01e1d8c5f316eb62facd3fb02f1043bc91b690d3
eslamelamin <sfirtalaslam@gmail.com>
2022-11-02 06:22:49 -0700

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _puts_recursion - print a string
- * @s:string 
+ * _puts_recursion - prints a string, followed by a new line
+ * @s:string
  * Return:void
  */
 void _puts_recursion(char *s)
