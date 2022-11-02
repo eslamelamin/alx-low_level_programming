d1b9788883c47e5af693d04b35730f8b6fbfa5e4
eslamelamin <sfirtalaslam@gmail.com>
2022-11-02 06:28:59 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,20 +6,13 @@
  */
 int factorial(int n)
 {
-	int x;
-
-	if (n == 0)
-	{
-		return (1);
-	}
-	else if (n < 0)
+	if (n < 0)
 	{
 		return (-1);
 	}
-	else
+	if (n == 0 || n == 1)
 	{
-		x = n * factorial(n - 1);
+		return (1);
 	}
-	return (x);
-
-}	
+	return (n * factorial(n - 1));
+}
